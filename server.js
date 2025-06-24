9d3f3c85738cdf60731baf944029f630be8a1abf
thiranity17 <thiranity17@gmail.com>
2025-06-24 19:42:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ const formRoutes = require('./routes/formRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-
-
 app.use(cors({
   origin: 'https://fiat-app1.web.app/',
   credentials: true
